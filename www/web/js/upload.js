f30636a00ad546d38bb112152ad14b9539857f07
sergey belov <limitium@gmail.com>
2013-02-15 00:58:50 +0400

    player loader

--- www/web/js/upload.js
+++ www/web/js/upload.js
@@ -1,35 +1,60 @@
-function onYouTubePlayerReady(playerId) {
-    player = document.getElementById("zebra");
-    console.log(player);
-//            player.playVideo();
-}
-function getVideoId() {
-    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
-
-    var match = $("#youtube-link").val().match(regExp);
-    console.log(match)
-    if (match && match[2].length == 11) {
-        return match[2];
+u = {
+    lastUrlChange: 0,
+    loadTimeOut: 500,
+    loadedVideoId: false,
+    player: null,
+    getVideoId: function () {
+        var regExp = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/;
+        var match = $("#youtube-link").val().match(regExp);
+        if (match && match[2].length == 11) {
+            return match[2];
+        }
+        return false;
+    },
+    loadVideo: function () {
+        if ((new Date()).getTime() - u.lastUrlChange >= u.loadTimeOut) {
+            var vid = u.getVideoId(), wrong = $("#wrong-url");
+            if (vid) {
+                if (u.loadedVideoId != vid) {
+                    u.loadedVideoId = vid;
+                    wrong.addClass("hide");
+                    swfobject.embedSWF(
+                        "http://www.youtube.com/v/" + vid + "?hl=en_US&fs=1&enablejsapi=1&playerapiid=ytplayer",
+                        "player",
+                        "100%", "300",
+                        "8",
+                        null, null,
+                        { allowScriptAccess: 'always' },
+                        { id: 'player-swf' }
+                    );
+                }
+            } else {
+                u.loadedVideoId = false;
+                u.player = null;
+                wrong.removeClass("hide");
+                $("#player-container").html('<div id="player"></div>');
+            }
+        }
+    },
+    onUrlChange: function () {
+        u.lastUrlChange = (new Date()).getTime();
+        setTimeout(u.loadVideo, u.loadTimeOut)
+    },
+    onPlayerLoad: function (player) {
+        u.player = player;
     }
-    return false;
-}
+};
 
-function loadVideo() {
-    var vid = getVideoId();
-    console.log(vid)
+function onYouTubePlayerReady(playerId) {
+    u.onPlayerLoad(document.getElementById("player-swf"));
 }
 
 $(function () {
-
-    $("#youtube-link").on("blur change keyup", loadVideo);
-
-    swfobject.embedSWF(
-        "http://www.youtube.com/v/qCTLCNmnlKU?hl=en_US&fs=1&enablejsapi=1&playerapiid=ytplayer",
-        "player",
-        "100%", "300",
-        "8",
-        null, null,
-        { allowScriptAccess: 'always' },
-        { id: 'zebra' }
-    );
+    $("#youtube-link").on("blur change keyup", u.onUrlChange);
+    $("#player-swf").load(function () {
+        console.log(this)
+    });
+    $("#player-container").on("load", "#player-swf", function () {
+        console.log(this)
+    })
 });

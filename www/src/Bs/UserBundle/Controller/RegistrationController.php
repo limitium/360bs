2b601fbffd711c9e2cd0f397b7860591035faef7
sergey belov <limitium@gmail.com>
2013-04-18 01:01:27 +0400

    register done

---
+++ www/src/Bs/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: limi
+ * Date: 17.04.13
+ * Time: 21:26
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Bs\UserBundle\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+
+class RegistrationController extends BaseController
+{
+    public function registerAction()
+    {
+        $form = $this->container->get('fos_user.registration.form');
+        $formHandler = $this->container->get('fos_user.registration.form.handler');
+
+        $process = $formHandler->process(false);
+        if ($process) {
+            $user = $form->getData();
+
+            $this->setFlash('success', 'registration.flash.user_created');
+
+            $url = $this->container->get('router')->generate('video', array('filter' => 'newset'));
+            $response = new RedirectResponse($url);
+
+            $this->authenticateUser($user, $response);
+
+            return $response;
+        }
+
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.' . $this->getEngine(), array(
+            'form' => $form->createView(),
+        ));
+    }
+}
